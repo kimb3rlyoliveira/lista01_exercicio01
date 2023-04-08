7290efb9b3f3dce68a88e845353bd2ec267bc193
kiimolliveiraa <kiimolliveiraa@gmail.com>
2023-04-07 23:41:05 -0300

    Primeiro commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Alô, Mundo!");
+import java.util.Scanner;
+
+class App {
+  
+  public static void main(String[] args) {
+    double numero;
+
+    System.out.print("Insira um número: ");
+    try (
+    Scanner teclado = new Scanner(System.in)) {
+      numero = teclado.nextDouble();
+    teclado.close();
     }
+
+    System.out.println("O número informado foi: " + numero);
+  }
+}
 }
